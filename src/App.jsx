880679d794ae49cb28de02911262d41db2424e06
Vivek Verma <vivek@example.com>
2026-07-07 11:10:37 +0530

    Add application entry points: main.jsx and App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect, useState } from 'react'
+import Login from './Componets/Auth/Login'
+import EmployeeDashboard from './Componets/Dashboard/EmployeeDashboard'
+import AdminDashboard from './Componets/Dashboard/AdminDashboard'
+import { AuthContext } from './context/AuthProvider'
+
+const App = () => {
+
+  const [user, setUser] = useState(null)
+  const [loggedInUserData, setLoggedInUserData] = useState(null)
+  const [userData,SetUserData] = useContext(AuthContext)
+
+  useEffect(()=>{
+    const loggedInUser = localStorage.getItem('loggedInUser')
+    
+    if(loggedInUser){
+      let userData
+      try {
+        userData = JSON.parse(loggedInUser)
+        setUser(userData.role)
+        setLoggedInUserData(userData.data)
+      } catch (error) {
+        console.error('Invalid localStorage data:', error)
+        localStorage.removeItem('loggedInUser')
+      }
+    }
+
+  },[])
+
+
+  const handleLogin = (email, password) => {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (userData) {
+      const employee = userData.find((e) => email == e.email && e.password == password)
+      if (employee) {
+        setUser('employee')
+        setLoggedInUserData(employee)
+        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee',data:employee }))
+      }
+    }
+    else {
+      alert("Invalid Credentials")
+    }
+  }
+
+
+
+  return (
+    <>
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null) }
+    </>
+  )
+}
+
+export default App
